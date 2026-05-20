35c3372394a71cb26cd23726766b64ef37aeb418
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-05-20 21:14:39 +0530

    calculator

--- cal.js
+++ cal.js
@@ -1,16 +1,11 @@
 
-
-
-
 document.addEventListener("DOMContentLoaded", function(){
-    let inp= document.querySelector("input")
+let inp= document.querySelector("input")
 let buttons=document.querySelectorAll(".button button")
 for(let btn of buttons){
   
      btn.addEventListener("click" ,()=>{
-    // console.log("hiii");
     let txt=btn.innerText
-    // console.log(txt);
     if(txt=="C"){
          inp.value=""
      }else if(txt=="AC"){
@@ -18,16 +13,14 @@ for(let btn of buttons){
      }else if(txt== "="){
           let expression = inp.value;
 
-    // 🔥 % ko /100 me convert karo (real calculator logic)
+    
     expression = expression.replace(/%/g,"/100");
 
     try{
         inp.value = eval(expression);
     }catch{
         inp.value = "Error";
-    }   //eval calculate a result
-    }else if(txt=="()"){
-    addBracket()
+    }   
     }else if(txt=="%"){
         inp.value+="%"
 
